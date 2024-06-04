2211e7520415ce8949c392a0e9bd3842c583f9b0
Jonathan Roy <jonaisroy@gmail.com>
2024-06-04 17:23:49 +0300

    function to loop through the array and display each book on the page

--- script.js
+++ script.js
@@ -19,11 +19,36 @@ function addBookToLibrary() {
   let title = prompt("Enter the title of the book:");
   let author = prompt("Enter the author of the book:");
   let pages = prompt("Enter the number of pages:");
-  let read = prompt("Have you read this book? (yes/no)").toLowerCase() === "yes";
+  let read =
+    prompt("Have you read this book? (yes/no)").toLowerCase();
 
   let newBook = new books(title, author, pages, read);
   myLibrary.push(newBook);
+  displayLibrary();
 }
 
 addBookToLibrary();
-console.log(myLibrary);
+
+function displayLibrary() {
+  const titleDiv = document.getElementById("title");
+  const authorDiv = document.getElementById("author");
+  const pagesDiv = document.getElementById("pages");
+  const readDiv = document.getElementById("read");
+
+  myLibrary.forEach((books) => {
+    const titles = document.createElement("p");
+    const authors = document.createElement("p");
+    const pages = document.createElement("p");
+    const reads = document.createElement("p");
+
+    titles.textContent = books.title;
+    authors.textContent = books.author;
+    pages.textContent = books.pages;
+    reads.textContent = books.read;
+
+    titleDiv.appendChild(titles);
+    authorDiv.appendChild(authors);
+    pagesDiv.appendChild(pages);
+    readDiv.appendChild(reads);
+  });
+}
